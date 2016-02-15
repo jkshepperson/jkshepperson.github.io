eb3bc296efdde22bfc29d43df5f336c8f97efa4b
Joell Shepperson <joellshepperson@Joells-MBP.home>
2016-02-15 16:40:36 -0500

    built out more pages + JS Pseudocode
    built out the ‘about’, ‘strategy’, ‘design’ and ‘contact’ pages
    roughly. created more concrete styles and nailed down a color palette
    for the site, created the outline of my JS interactions i want to try

---
+++ joellportfolio/js/portfolio.js
@@ -0,0 +1,22 @@
+//About page
+//when user clicks the article links or book links, open full URL in new tab
+
+//Artifacts Page
+//when user clicks on image of artifact
+//display the image in a modal layer pop up with discription
+//also show an 'x' that when clicked, will close the pop up and remove the modal layer
+
+//Design Page
+//when user clicks on image from the design project
+//expand the project section to fill the entire page
+//show images in carousel view with arrows that will flip through the images
+//change the background color to white
+//also show an 'x' in the upper right hand corner of the expanded window
+//when user clicks on the 'x' return to normal view of project
+
+//Contact page
+//create validation on the form fields
+//on submit, create email and send to jkshepperson@gmail.com 
+//on submit, remove the form and instead display large text 'Thank you!'
+//only allow one submission per email address
+
