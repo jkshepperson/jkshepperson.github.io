1049d2e352f1db73c95c416c2a78bdef2b41ae13
Joell Shepperson <joellshepperson@Joells-MBP.home>
2016-02-21 22:11:27 -0500

    lots of work
    lots of tweaking on the process and content pages. need to clean the
    padding/spacing issues + js show/hide event.

--- joellportfolio/js/portfolio.js
+++ joellportfolio/js/portfolio.js
@@ -3,32 +3,43 @@ $(document).ready(function() {
 //About page
 //when user clicks the article links or book links, open full URL in new tab
 
-//Artifacts Page
+//Artifacts + Design Page (image behavior)
 
 $('img').click(blowUp); // when user clicks on images, blow them up by toggling between classes
 
 function blowUp() { 
-	$('.thumbnails').toggleClass('large');// when a user clicks an image blow up
+	$(this).toggleClass('large');// when a user clicks an image blow up
+}
+
+//Process Page
+
+$('#showMore').click(showMoreStuff); //when user clicks on project section (between the divs)
+
+function showMoreStuff () {
+	event.preventDefault();
+	$('img').addClass('large'); //blowup the images
+	$('#show-on-click').slideDown(); //push other sections down, reveal hidden text
+	$('#showLess').slideDown(); //push the show less link down, too
+	$('#showLess').show(); //show the show less link
+	$('#showMore').hide(); //hide the show more link
+
+}
+
+$('#showLess').click(showLessStuff); //when user clicks show less, collapse stuff
+
+function showLessStuff () {
+	event.preventDefault();
+	$('img').removeClass('large');
+	$('#show-on-click').slideUp(); //collapse section up
+	$('#showLess').slideUp(); //collapse link up
+	$('#showLess').hide(); //hide show less link
+	$('#showMore').show(); //show 'show more' link
 }
 
 
 
-
-/*
-    Love the pseudocode here.  Would love to see this fleshed out into JS code
-*/
-
-//Design Page
-//when user clicks on image from the design project
-//expand the project section to fill the entire page
-//show images in carousel view with arrows that will flip through the images
-//change the background color to white
-//also show an 'x' in the upper right hand corner of the expanded window
-//when user clicks on the 'x' return to normal view of project
-
 //Contact page
-//create validation on the form fields
-//on submit, create email and send to jkshepperson@gmail.com 
-//on submit, remove the form and instead display large text 'Thank you!'
-//only allow one submission per email address
+//when a user clicks one of the titles
+//turn that box green with a check mark
+//display thank you text underneath
 });
